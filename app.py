648fa4cb86d033ad4c1381292dfa86a0aade317b
Ana Jessica <anajessica15022002.com>
2022-01-19 18:27:27 +0530

    changes

--- app.py
+++ app.py
@@ -5,27 +5,21 @@ import os
 
 load_dotenv()
 
-
 LOCAL_FILE_PATH = os.environ.get('LOCAL_FILE_PATH')
-ACCESS_KEY = os.environ.get('ACCESS_KEY')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+ACCESS_KEY      = os.environ.get('ACCESS_KEY')
+SECRET_KEY      = os.environ.get('SECRET_KEY')
 
 MINIO_API_HOST = "http://localhost:9000"
 
-MINIO_CLIENT = Minio(
-        "localhost:9000", access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False   
-    )
+MINIO_CLIENT = Minio("localhost:9000", access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
 
 BUCKET_NAME = "first"
-
-
 
 app = Flask(__name__)
 
 def get_all_images():
 
     images = []
-
 
     for single_object in MINIO_CLIENT.list_objects(BUCKET_NAME, recursive=True):
 
@@ -39,7 +33,6 @@ def get_all_images():
     return images
 
 def get_all_videos():
-
     
     videos=[]
 
@@ -53,26 +46,22 @@ def get_all_videos():
     print(videos)
     return videos
 
-def main():
-
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    
     found = MINIO_CLIENT.bucket_exists("first")
+
     if not found:
         MINIO_CLIENT.make_bucket("first")
     else:
         print("Bucket 'first' already exists")
 
-    MINIO_CLIENT.fput_object(
-        "first", "pic.jpg", LOCAL_FILE_PATH, 
-    )
-    print(
-       "It is successfully uploaded as "
-        "object 'bbb.jpg' to bucket 'first'."
-    )
+    MINIO_CLIENT.fput_object("first", "pic.jpg", LOCAL_FILE_PATH,)
+    
+    print("It is successfully uploaded")
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():
-    main()
     all_images = get_all_images()
+
     all_videos = get_all_videos()
 
     return render_template('index.html', images = all_images, videos =all_videos)
